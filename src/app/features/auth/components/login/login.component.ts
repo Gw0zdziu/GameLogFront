74607a63954059014229e283697ba4ae3b9da618
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-16 15:22:06 +0200

    feat(components): inject service to component

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,16 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {InputText} from 'primeng/inputtext';
-import {Button} from 'primeng/button';
+import {ButtonDirective, ButtonLabel} from 'primeng/button';
+import {AuthService} from '../../services/auth/auth.service';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LoginUserDto} from '../../../shared/models/request/login-user.dto';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
     InputText,
-    Button
+    ReactiveFormsModule,
+    ButtonLabel,
+    ButtonDirective
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+  isLogin = signal(false);
+  loginForm = this.formBuilder.group({
+    userName: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
+  loginUser(): void{
+    this.isLogin.set(true);
+    const loginUser: LoginUserDto = this.loginForm.value as LoginUserDto;
+    this.authService.loginUser(loginUser)
+      .subscribe({
+      next: () => {
+        this.router.navigate(['']);
+      },
+      error: () => {
+        this.isLogin.set(false);
+      },
+      complete: () => this.isLogin.set(false)
+    })
+  }
 }
