011a8522a541ae20e11e8070d6f5373be1431c7f
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-18 23:24:37 +0200

    feat(components): update home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {UserStoreService} from '../shared/services/store/user-store/user-store.service';
+import {UserService} from '../auth/services/user/user.service';
+import {AuthService} from '../auth/services/auth/auth.service';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterLink
+    RouterLink,
+    Button,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  private userStoreService = inject(UserStoreService);
+  private userService = inject(UserService);
+  private authService = inject(AuthService);
+  user$ = this.userStoreService.currentUser;
 
+  ngOnInit(): void {
+    const userId = this.userStoreService.currentUser()?.userId as string;
+    if (userId) {
+      this.userService.getUser(userId).subscribe({
+        next: (value) => {
+          this.userStoreService.updateUser(value);
+        }
+      })
+    }
+  }
+
+  logout(){
+    const userId = this.userStoreService.currentUser()?.userId as string;
+    this.authService.logoutUser(userId).subscribe({
+      next: () => {
+        this.userStoreService.cleanStore()
+      }
+    })
+  }
 }
