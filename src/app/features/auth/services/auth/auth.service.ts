34d5e56d92ed2e90517c1dbc737a8f068a2f70b3
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-18 23:21:49 +0200

    feat(services): add method to logout user and update login user method

--- src/app/features/auth/services/auth/auth.service.ts
+++ src/app/features/auth/services/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {environment} from '../../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {LoginUserDto} from '../../../shared/models/request/login-user.dto';
+import {LoginResponseDto} from '../../../shared/models/response/login-response.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,12 @@ export class AuthService {
   private apiUrl = `${environment.apiUrl}/auth`
   private httpClient = inject(HttpClient);
 
-  loginUser(loginUser: LoginUserDto): Observable<string> {
-    return this.httpClient.post(`${this.apiUrl}/login`, loginUser, {
-      responseType: 'text'
-    });
+  loginUser(loginUser: LoginUserDto): Observable<LoginResponseDto> {
+    return this.httpClient.post<LoginResponseDto>(`${this.apiUrl}/login`, loginUser);
+  }
+
+   logoutUser(userId: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/logout/${userId}`);
   }
 
 
