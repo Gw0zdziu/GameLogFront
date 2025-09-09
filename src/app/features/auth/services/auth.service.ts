0eeeaceda09d335221232cbef3bca2de06922477
gw0zdziu <gw0zdziu09@gmail.com>
2025-09-09 13:44:30 +0200

    feat(services): add HttpContext class to getUser, logoutUser and refreshToken method

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '../../../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpContext} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {LoginUserDto} from '../models/login-user.dto';
 import {LoginResponseDto} from '../models/login-response.dto';
+import {IS_AUTH_REQUIRED} from '../../../core/tokens/tokens';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +16,14 @@ export class AuthService {
   loginUser(loginUser: LoginUserDto): Observable<LoginResponseDto> {
     return this.httpClient.post<LoginResponseDto>(`${this.apiUrl}/login`, loginUser, {
       withCredentials: true,
+      context: new HttpContext().set(IS_AUTH_REQUIRED, true)
     });
   }
 
    logoutUser(userId: string): Observable<void> {
     return this.httpClient.delete<void>(`${this.apiUrl}/logout/${userId}`, {
       withCredentials: true,
+      context: new HttpContext().set(IS_AUTH_REQUIRED, true)
     });
   }
 
