bd41ba6e5bc5908fdb5d56ca99aa6b1cf076333f
gw0zdziu <gw0zdziu09@gmail.com>
2025-09-19 20:04:09 +0200

    fix(interceptors): add condition if user is logged

--- src/app/core/interceptors/refresh-token/refresh-token.interceptor.ts
+++ src/app/core/interceptors/refresh-token/refresh-token.interceptor.ts
@@ -2,18 +2,24 @@ import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
 import {UserStoreService} from '../../store/user-store/user-store.service';
 import {inject} from '@angular/core';
 import {RefreshTokenService} from '../../services/refresh-token/refresh-token.service';
-import {catchError, switchMap} from 'rxjs';
+import {catchError, map, of} from 'rxjs';
+import {LoggedStoreService} from '../../store/logged-store/logged-store.service';
 
 export const refreshTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const userStoreService = inject(UserStoreService);
   const refreshTokenService = inject(RefreshTokenService);
+  const loggedStoreService = inject(LoggedStoreService);
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
-      if (err.status === 401) {
+      if (err.status === 401 && loggedStoreService.isLogged$()) {
       return refreshTokenService.refreshToken().pipe(
-        switchMap(x => {
-            userStoreService.updateUser({token: x});
-            return next(req);
+        catchError((err, caught) => {
+          userStoreService.cleanStore();
+          return of(err)
+        }),
+        map(x => {
+          userStoreService.updateUser({token: x});
+          return x
         })
       )} else {
         return next(req);
