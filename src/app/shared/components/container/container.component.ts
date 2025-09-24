38c77d14a35a6404a10cf5c9df3c6782bbaba8ff
gw0zdziu <gw0zdziu09@gmail.com>
2025-09-24 12:19:35 +0200

    refactor(components): move template html into component

--- src/app/shared/components/container/container.component.ts
+++ src/app/shared/components/container/container.component.ts
@@ -3,7 +3,11 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-container',
   imports: [],
-  templateUrl: './container.component.html',
+  template: `
+    <div class="container">
+      <ng-content></ng-content>
+    </div>
+  `,
   styleUrl: './container.component.css'
 })
 export class ContainerComponent {
