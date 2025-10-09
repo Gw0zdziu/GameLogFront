69c28488c961d63d4cbfe4f5d1754384baa35410
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-09 15:03:00 +0200

    feat(components): inject table of categories into main component category

--- src/app/features/category/category.component.ts
+++ src/app/features/category/category.component.ts
@@ -1,11 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {CategoryService} from './services/category.service';
+import {TableModule} from 'primeng/table';
+import {CategoryDto} from './models/category.dto';
+import {ButtonDirective, ButtonLabel} from 'primeng/button';
+import {DialogService, DynamicDialogRef} from 'primeng/dynamicdialog';
+import {CategoryTableComponent} from './components/category-table/category-table.component';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [
+    TableModule,
+    ButtonDirective,
+    ButtonLabel,
+    CategoryTableComponent
+  ],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
+  providers: [DialogService],
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+  private categoryService = inject(CategoryService);
+  private dialogService = inject(DialogService);
+  categories = signal<CategoryDto[]>([]);
+  ref: DynamicDialogRef | undefined;
+
+
+  ngOnInit(): void {
+    this.categoryService.getUserCategories().subscribe({
+      next: (response) => {
+        this.categories.set(response);
+      },
+    });
+  }
+
+
 
 }
