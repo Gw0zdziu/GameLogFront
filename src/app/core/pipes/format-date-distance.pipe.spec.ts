5a6b2014e7acf973d1f430a935a9bab10b7f829a
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-09 15:00:32 +0200

    feat(pipes): implement pipe to format date creating and updating category

---
+++ src/app/core/pipes/format-date-distance.pipe.spec.ts
@@ -0,0 +1,8 @@
+import {FormatDateDistancePipe} from './format-date-distance.pipe';
+
+describe('FormatDateDistancePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatDateDistancePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
