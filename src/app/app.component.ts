a57c3ce767b02c8cee7416e112f7c118f2299a01
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-20 13:15:59 +0200

    feat(components): add confirm dialog to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,21 @@ import {AuthService} from './features/auth/services/auth.service';
 import {ThemeStoreService} from './core/store/theme-store/theme-store.service';
 import {DOCUMENT} from '@angular/common';
 import {Toast} from 'primeng/toast';
+import {ConfirmDialog} from 'primeng/confirmdialog';
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Toast
+    Toast,
+    ConfirmDialog,
+    TableModule
   ],
   template: `
   <router-outlet/>
   <p-toast position="bottom-center"></p-toast>
+  <p-confirmdialog></p-confirmdialog>
   `,
   styleUrl: './app.component.css',
 })
@@ -21,11 +26,17 @@ export class AppComponent implements OnInit{
   title = 'GameLogFront';
   private authService = inject(AuthService);
   private themeStore = inject(ThemeStoreService);
-  theme = this.themeStore.theme$();
+  themeState$ = this.themeStore.theme$();
   private document = inject(DOCUMENT);
   private renderer = inject(Renderer2);
 
   ngOnInit(): void {
-    this.renderer.setAttribute(this.document.documentElement,'data-theme', this.theme.theme);
+    this.renderer.setAttribute(this.document.documentElement,'dark-theme', this.themeState$.theme);
+    const element = this.document.querySelector('html') as HTMLHtmlElement;
+    if (this.themeState$.theme === 'dark') {
+      element.classList.add(this.themeState$.theme);
+    } else {
+      element.classList.remove(this.themeState$.theme);
+    }
   }
 }
