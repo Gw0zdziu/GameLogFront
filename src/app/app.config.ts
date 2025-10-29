998b1e458ff5c2747a8b6bf39a428492819dfa58
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-30 00:37:06 +0100

    feat(config): provide service to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import {authInterceptor} from './core/interceptors/auth/auth.interceptor';
 import {refreshTokenInterceptor} from './core/interceptors/refresh-token/refresh-token.interceptor';
 import {definePreset} from '@primeng/themes';
 import {ConfirmationService, MessageService} from 'primeng/api';
+import {FormatDateDistancePipe} from './core/pipes/format-date-distance.pipe';
 
 const Preset = definePreset(Aura, {
   components: {
@@ -86,6 +87,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor, refreshTokenInterceptor])),
     provideRouter(routes),
     provideAnimationsAsync(),
+    FormatDateDistancePipe,
     providePrimeNG({
       theme: {
         preset: Preset,
