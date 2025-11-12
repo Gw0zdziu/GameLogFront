cff21354ccb85a651c1612eccd7c60bbe00fa504
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-12 20:33:31 +0100

    refactor(components): minor updates and fixes

--- src/app/core/interceptors/refresh-token/refresh-token.interceptor.ts
+++ src/app/core/interceptors/refresh-token/refresh-token.interceptor.ts
@@ -2,30 +2,39 @@ import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
 import {UserStoreService} from '../../store/user-store/user-store.service';
 import {inject} from '@angular/core';
 import {RefreshTokenService} from '../../services/refresh-token/refresh-token.service';
-import {catchError, map, of} from 'rxjs';
+import {catchError, switchMap, throwError} from 'rxjs';
 import {LoggedStoreService} from '../../store/logged-store/logged-store.service';
 
 export const refreshTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const userStoreService = inject(UserStoreService);
   const refreshTokenService = inject(RefreshTokenService);
   const loggedStoreService = inject(LoggedStoreService);
+  let isRefreshing = false;
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
-      if (err.status === 401 && loggedStoreService.isLogged$()) {
-      return refreshTokenService.refreshToken().pipe(
-        map(x => {
-          userStoreService.updateUser({token: x});
-          loggedStoreService.setLogged(true);
-          return next(req)
-        }),
-        catchError((err) => {
-          userStoreService.cleanStore();
-          loggedStoreService.setLogged(false);
-          return of(err)
-        })
-      )} else {
-        return next(req);
+      if (err.status === 401) {
+        if (!isRefreshing) {
+          if (loggedStoreService.isLogged$()) {
+            return refreshTokenService.refreshToken().pipe(
+              switchMap(token => {
+                userStoreService.updateUser({token});
+                req = req.clone({
+                  setHeaders: {
+                    Authorization: `Bearer ${token}`
+                  }
+                })
+                return next(req);
+              }),
+              catchError(err => {
+                return throwError(() => err)
+              })
+            )
+          }
+        }
       }
+      return throwError(() => err)
     })
   )
 };
+
+
--- src/app/features/game/components/game-form/game-form.component.ts
+++ src/app/features/game/components/game-form/game-form.component.ts
@@ -1,6 +1,6 @@
-import {ChangeDetectionStrategy, Component, effect, inject, input, OnInit, output, signal} from '@angular/core';
+import {AfterViewInit, Component, effect, inject, input, OnInit, output, signal, ViewChild} from '@angular/core';
 import {AutoComplete, AutoCompleteCompleteEvent} from 'primeng/autocomplete';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, NgModel, ReactiveFormsModule} from '@angular/forms';
 import {CategoryDto} from '../../../category/models/category.dto';
 import {CategoryStore} from '../../../category/store/category-store';
 import {GameBaseDto} from '../../models/game-base.dto';
@@ -19,19 +19,22 @@ import {GameStore} from '../../store/game-store';
     InputText,
     ButtonDirective,
     ButtonLabel,
+
+
   ],
   templateUrl: './game-form.component.html',
   styleUrl: './game-form.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GameFormComponent<T extends GameBaseDto> implements OnInit{
+export class GameFormComponent<T extends GameBaseDto> implements OnInit, AfterViewInit{
   private categoryStore = inject(CategoryStore);
   gameStore = inject(GameStore);
-  updatedGame = input.required<T | null>();
   gameName = signal('');
   selectedCategory = signal<CategoryDto | null>(null);
   filteredCategories = signal<CategoryDto[]>([]);
+  isSelectCategory = signal(true);
   onSubmit = output<T>();
+  updatedGame = input.required<T | null>();
+  @ViewChild('category') categoryInput!: NgModel;
 
 
   constructor() {
@@ -39,6 +42,12 @@ export class GameFormComponent<T extends GameBaseDto> implements OnInit{
       this.selectedCategory.set(this.categoryStore.categories().find(x => x.categoryId === this.updatedGame()?.categoryId) as CategoryDto);
       this.gameName.set(this.updatedGame()?.gameName as string)
     });
+  }
+
+  ngAfterViewInit() {
+    if (this.categoryInput){
+      console.log(this.categoryInput)
+    }
   }
 
   ngOnInit(): void {
@@ -49,6 +58,8 @@ export class GameFormComponent<T extends GameBaseDto> implements OnInit{
 
   filterCategory($event: AutoCompleteCompleteEvent) {
     this.filteredCategories.set(this.categoryStore.categories().filter(category => category.categoryName.toLowerCase().includes( $event.query.toLowerCase())));
+    this.isSelectCategory.set(this.filteredCategories().length === 0);
+
   }
 
   formSubmit() {
@@ -57,5 +68,14 @@ export class GameFormComponent<T extends GameBaseDto> implements OnInit{
       categoryId: this.selectedCategory()?.categoryId as string,
     } as T;
     this.onSubmit.emit(newGame);
+  }
+
+   selectCategory() {
+     this.isSelectCategory.set(true);
+  }
+
+
+  protected onChangeInput() {
+    this.isSelectCategory.set(typeof this.categoryInput.value === 'object');
   }
 }
