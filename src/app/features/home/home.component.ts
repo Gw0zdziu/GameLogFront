59f0ffb0c8b87cb9e5a100090867ada65f0ba26a
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-30 17:43:48 +0100

    style(home): apply OnPush change detection and improve formatting
    - Enabled `ChangeDetectionStrategy.OnPush` for better performance.
    - Adjusted code formatting for consistency.
    - Updated template to use self-closing components and reorganized imports.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,28 +1,29 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {UserService} from '../user/services/user.service';
-import {NavbarComponent} from '../../core/components/navbar/navbar.component';
-import {LayoutService} from '../../shared/services/layout/layout.service';
-import {MenuComponent} from '../../core/components/menu/menu.component';
-import {RouterOutlet} from '@angular/router';
-import {LoggedStoreService} from '../../core/store/logged-store/logged-store.service';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { UserService } from '../user/services/user.service';
+import { LayoutService } from '../../shared/services/layout/layout.service';
+import { MenuComponent } from '../../core/components/menu/menu.component';
+import { RouterOutlet } from '@angular/router';
+import { LoggedStoreService } from '../../core/store/logged-store/logged-store.service';
+import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NavbarComponent,
     MenuComponent,
     RouterOutlet,
+    NavbarComponent
   ],
   template: `
-    <app-navbar/>
+    <header app-navbar></header>
     <section class="container">
-      <app-menu/>
-      <div class="content" [class.expanded]="isMenuOpen$()" >
-        <router-outlet></router-outlet>
+      <app-menu />
+      <div class="content" [class.expanded]="isMenuOpen$()">
+        <router-outlet />
       </div>
     </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit{
   private userService = inject(UserService);
