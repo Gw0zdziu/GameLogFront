58f9e3b2695d1efea3c736948de63cded81e0c4e
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-30 17:44:29 +0100

    style(theme-toggle): enable OnPush change detection and improve structure
    - Applied `ChangeDetectionStrategy.OnPush` for enhanced performance.
    - Reorganized imports for better readability and consistency.
    - Added `ariaLabel` for improved accessibility.
    - Updated template for cleaner structure.
    - Specified return type for `toggleTheme()` method.

--- src/app/features/theme-toggle/theme-toggle.component.ts
+++ src/app/features/theme-toggle/theme-toggle.component.ts
@@ -1,15 +1,16 @@
-import {Component, inject, Renderer2} from '@angular/core';
-import {Button} from 'primeng/button';
-import {DOCUMENT} from '@angular/common';
-import {ThemeStoreService} from '../../core/store/theme-store/theme-store.service';
+import { ChangeDetectionStrategy, Component, inject, Renderer2 } from '@angular/core';
+import { Button } from 'primeng/button';
+import { DOCUMENT } from '@angular/common';
+import { ThemeStoreService } from '../../core/store/theme-store/theme-store.service';
 
 @Component({
-  selector: 'app-theme-toggle',
+  changeDetection: ChangeDetectionStrategy.OnPush,selector: 'app-theme-toggle',
   imports: [
     Button
   ],
   template:`
-    <p-button (click)="toggleTheme()" class="theme-toggle" icon="pi pi-{{theme().icon}}" [rounded]="true" [text]="true"/>
+    <p-button ariaLabel="Toggle Theme Button" class="theme-toggle" icon="pi pi-{{theme().icon}}"
+              [rounded]="true" [text]="true" (click)="toggleTheme()"/>
   `,
   styleUrl: './theme-toggle.component.css'
 })
@@ -20,7 +21,7 @@ export class ThemeToggleComponent {
   theme = this.themeStore.theme$;
 
 
-  toggleTheme() {
+  toggleTheme(): void {
     const currentTheme = !this.theme().isDark ? 'dark' : 'light';
     this.renderer.setAttribute(this.document.documentElement,'dark-theme', currentTheme);
     const element = this.document.querySelector('html') as HTMLHtmlElement;
