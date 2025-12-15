07f3c164ee197b5dd3be1571f50db21e7d94784d
gw0zdziu <gw0zdziu09@gmail.com>
2025-12-15 13:16:09 +0100

    test(game-add.component): add unit tests for `submitNewGame` method
    - Mocked dependencies including `DynamicDialogRef` and `GameStore`.
    - Verified `postGame` was called with the correct parameters.
    - Ensured `close` method was triggered after successful submission.

--- src/app/features/game/components/game-add/game-add.component.spec.ts
+++ src/app/features/game/components/game-add/game-add.component.spec.ts
@@ -1,14 +1,47 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {GameAddComponent} from './game-add.component';
+import {DynamicDialogRef} from 'primeng/dynamicdialog';
+import {GameStore} from '../../store/game-store';
+import {ChangeDetectionStrategy, Component, output, signal} from '@angular/core';
+import {GamePostDto} from '../../models/game-post.dto';
+import {GameFormComponent} from '../game-form/game-form.component';
+
+@Component({
+  selector: 'app-game-form',
+  template: '',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+class MockGameFormComponent {
+  readonly submitEmitter = output<GamePostDto>();
+}
 
 describe('GameAddComponent', () => {
   let component: GameAddComponent;
   let fixture: ComponentFixture<GameAddComponent>;
-
+  let dynamicDialogRefMock: jest.Mocked<Partial<DynamicDialogRef>>;
+  const gameStoreMock = {
+    postGame: jest.fn(),
+    isLoading: signal(false),
+  }
   beforeEach(async () => {
+    dynamicDialogRefMock = {
+      close: jest.fn(),
+    }
     await TestBed.configureTestingModule({
-      imports: [GameAddComponent]
+      imports: [GameAddComponent],
+      providers: [
+        {provide: DynamicDialogRef, useValue: dynamicDialogRefMock},
+        {provide: GameStore, useValue: gameStoreMock},
+      ]
+    }).overrideComponent(GameAddComponent, {
+      remove: {
+        imports: [GameFormComponent]
+      },
+      add: {
+        imports: [MockGameFormComponent]
+      }
     })
     .compileComponents();
 
@@ -17,7 +50,26 @@ describe('GameAddComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('submitNewGame()', () =>{
+    it('should called postGame from gameStore when execute submitNewGame method', () => {
+      const newGame: GamePostDto = {
+        gameName: 'RPG',
+        categoryId: 'cat-001',
+      }
+      component.submitNewGame(newGame);
+      expect(gameStoreMock.postGame).toHaveBeenCalledWith({newGame, onSuccess: expect.any(Function)});
+    });
+
+    it('should called close method', () => {
+      const newGame: GamePostDto = {
+        gameName: 'RPG',
+        categoryId: 'cat-001',
+      }
+      gameStoreMock.postGame.mockImplementation((param) => {
+        param.onSuccess();
+      })
+      component.submitNewGame(newGame);
+      expect(dynamicDialogRefMock.close).toHaveBeenCalledWith(true);
+    });
+  })
 });
