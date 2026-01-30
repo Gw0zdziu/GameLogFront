0d007e5810284f4e2b7a1472d51cd678a7de54ce
przemekgwozdz <gw0zdziu09@gmail.com>
2026-01-30 17:08:36 +0100

    fix(locale): add locale property to language definitions

--- src/app/shared/constants/languages.ts
+++ src/app/shared/constants/languages.ts
@@ -1,12 +1,16 @@
 import {Language} from '../models/language';
+import {enGB} from 'date-fns/locale/en-GB';
+import {pl} from 'date-fns/locale/pl';
 
 export  const languages: Language[]  = [
   {
     langCode: 'pl',
-    langName: $localize`Polski`
+    langName: $localize`Polski`,
+    locale: pl
   },
   {
     langCode: 'en',
-    langName: $localize`Angielski`
+    langName: $localize`Angielski`,
+    locale: enGB
   }
 ]
