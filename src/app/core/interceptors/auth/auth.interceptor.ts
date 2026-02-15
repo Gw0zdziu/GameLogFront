a80d816b2148364e10ed73ca2afe1a6f5176eb87
przemekgwozdz <gw0zdziu09@gmail.com>
2026-02-15 18:21:35 +0100

    refactor(auth): replace UserStoreService with TokenStoreService in auth interceptor

--- src/app/core/interceptors/auth/auth.interceptor.ts
+++ src/app/core/interceptors/auth/auth.interceptor.ts
@@ -1,15 +1,15 @@
 import {HttpInterceptorFn} from '@angular/common/http';
 import {inject} from '@angular/core';
-import {UserStoreService} from '../../store/user-store/user-store.service';
 import {IS_AUTH_REQUIRED} from "../../tokens/tokens";
+import {TokenStoreService} from '../../store/token-store/token-store.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const userStoreService = inject(UserStoreService);
+  const userStoreService = inject(TokenStoreService);
   if(req.context.get(IS_AUTH_REQUIRED)){
-    if (userStoreService.user$()?.token) {
+    if (userStoreService.token$()) {
       const request = req.clone({
         setHeaders: {
-          Authorization: `Bearer ${userStoreService.user$()?.token}`
+          Authorization: `Bearer ${userStoreService.token$()}`
         }
       });
       return next(request);
