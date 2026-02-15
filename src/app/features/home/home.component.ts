d49993052fc6cd485f2c529aa2f58fab1e3dbd77
przemekgwozdz <gw0zdziu09@gmail.com>
2026-02-15 18:25:12 +0100

    refactor(home): remove unused services and simplify HomeComponent logic

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,7 @@
-import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
-import {UserService} from '../user/services/user.service';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {LayoutService} from '../../shared/services/layout/layout.service';
 import MenuComponent from '../../core/components/menu/menu.component';
 import {RouterOutlet} from '@angular/router';
-import {LoggedStoreService} from '../../core/store/logged-store/logged-store.service';
 import {NavbarComponent} from '../../core/components/navbar/navbar.component';
 
 @Component({
@@ -18,7 +16,7 @@ import {NavbarComponent} from '../../core/components/navbar/navbar.component';
     <header app-navbar></header>
     <section class="container">
       <app-menu  />
-      <div class="content" [class.expanded]="isMenuOpen$()">
+      <div class="content" [class.expanded]="layoutService.isMenuOpen$()">
         <router-outlet />
       </div>
     </section>
@@ -26,15 +24,6 @@ import {NavbarComponent} from '../../core/components/navbar/navbar.component';
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit{
-  private userService = inject(UserService);
-  private layoutService = inject(LayoutService);
-  private loggedStoreService = inject(LoggedStoreService);
-  isMenuOpen$ = this.layoutService.isMenuOpen$;
-
-  ngOnInit(): void {
-    if (this.loggedStoreService.isLogged$()) {
-      this.userService.getUser().subscribe();
-    }
-  }
+export class HomeComponent {
+  protected layoutService = inject(LayoutService);
 }
