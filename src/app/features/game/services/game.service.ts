f87c66fb6086542c57467d64a92bfee9bae405bc
przemekgwozdz <gw0zdziu09@gmail.com>
2026-02-17 18:03:59 +0100

    feat(game-service): add pagination support to `getUserGames` method

--- src/app/features/game/services/game.service.ts
+++ src/app/features/game/services/game.service.ts
@@ -1,11 +1,13 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '../../../../environments/environment';
-import {HttpClient, HttpContext} from '@angular/common/http';
+import {HttpClient, HttpContext, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {GameDto} from '../models/game.dto';
 import {IS_AUTH_REQUIRED} from '../../../core/tokens/tokens';
 import {GamePostDto} from '../models/game-post.dto';
 import {GamePutDto} from '../models/game-put.dto';
+import {PaginatedResults} from '../../../shared/models/paginated-results';
+import {PaginatedQuery} from '../../../shared/models/paginated-query';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +17,14 @@ export class GameService {
   private http = inject(HttpClient);
 
 
-  getUserGames(): Observable<GameDto[]>{
-    return this.http.get<GameDto[]>(`${this.apiUrl}/get-games`, {
+  getUserGames(paginatedQuery: PaginatedQuery): Observable<PaginatedResults<GameDto>>{
+    const query = new HttpParams()
+      .set('pageNumber', paginatedQuery.pageNumber)
+      .set('pageSize', paginatedQuery.pageSize)
+    return this.http.get<PaginatedResults<GameDto>>(`${this.apiUrl}/get-games`, {
       withCredentials: true,
-      context: new HttpContext().set(IS_AUTH_REQUIRED, true)
+      context: new HttpContext().set(IS_AUTH_REQUIRED, true),
+      params: query
     })
    }
 
