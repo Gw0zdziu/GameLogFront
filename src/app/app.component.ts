956d5eddc38b91c80744a27750f32d3a9223bb6e
przemekgwozdz <gw0zdziu09@gmail.com>
2026-02-18 15:27:40 +0100

    refactor(app): remove unused AuthService and UserService injections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import {DOCUMENT} from '@angular/common';
 import {Toast} from 'primeng/toast';
 import {ConfirmDialog} from 'primeng/confirmdialog';
 import {TableModule} from 'primeng/table';
-import {AuthService} from './features/auth/services/auth.service';
-import {UserService} from './features/user/services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -29,9 +27,6 @@ export class AppComponent implements OnInit{
   themeState$ = this.themeStore.theme$();
   private document = inject(DOCUMENT);
   private renderer = inject(Renderer2);
-  private authService = inject(AuthService);
-  private userService = inject(UserService);
-
   constructor(
     @Inject(LOCALE_ID) public activeLocale: string
   ) {
