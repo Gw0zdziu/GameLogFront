87fd46d3e5214a34a4064ef5e52e8fe17a5a8ec3
przemekgwozdz <gw0zdziu09@gmail.com>
2026-02-18 15:26:56 +0100

    refactor(logged-store): update default `isLogged` signal value and remove unused constructor logic

--- src/app/core/store/logged-store/logged-store.service.ts
+++ src/app/core/store/logged-store/logged-store.service.ts
@@ -4,14 +4,10 @@ import {computed, Injectable, signal} from '@angular/core';
   providedIn: 'root'
 })
 export class LoggedStoreService {
-  private readonly isLogged = signal<boolean | null>(null);
+  private readonly isLogged = signal<boolean | null>(false);
   readonly isLogged$ = computed(() => {
     return this.isLogged();
   })
-
-  constructor() {
-    console.log(this.isLogged())
-  }
 
   setLogged(value: boolean): void{
     this.isLogged.set(value);
