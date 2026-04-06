db34a5c79973c717971f077899b4147ac7b96e62
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-06 12:48:34 +0200

    chore(app): add loading spinner and integrate LoggedStoreService for login state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component, Inject, inject, LOCALE_ID, OnInit, Renderer2} from '@angular/core';
+import {Component, Inject, inject, LOCALE_ID, OnInit, Renderer2, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ThemeStoreService} from './core/store/theme-store/theme-store.service';
 import {DOCUMENT} from '@angular/common';
 import {Toast} from 'primeng/toast';
 import {ConfirmDialog} from 'primeng/confirmdialog';
 import {TableModule} from 'primeng/table';
+import {ProgressSpinner} from 'primeng/progressspinner';
+import {LoggedStoreService} from './core/store/logged-store/logged-store.service';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +14,19 @@ import {TableModule} from 'primeng/table';
     RouterOutlet,
     Toast,
     ConfirmDialog,
-    TableModule
+    TableModule,
+    ProgressSpinner,
   ],
   template: `
+    @if (loading$()) {
+      <div class="spinner">
+        <p-progress-spinner ariaLabel="loading" />
+      </div>
+    } @else {
     <router-outlet/>
     <p-toast position="bottom-center" [life]="2000"/>
     <p-confirmdialog ariaLabel="Confirm dialog" />
+    }
   `,
   styleUrl: './app.component.css',
 })
@@ -27,12 +36,19 @@ export class AppComponent implements OnInit{
   themeState$ = this.themeStore.theme$();
   private document = inject(DOCUMENT);
   private renderer = inject(Renderer2);
+
   constructor(
     @Inject(LOCALE_ID) public activeLocale: string
   ) {
   }
+  store = inject(LoggedStoreService)
+  isLogged$ = this.store.isLogged$;
+  readonly loading$ = signal<boolean>(true)
 
   ngOnInit(): void {
+    if (this.loading$ !== null){
+      this.loading$.set(false)
+    }
     this.renderer.setAttribute(this.document.documentElement,'dark-theme', this.themeState$.theme);
     const element = this.document.querySelector('html') as HTMLHtmlElement;
     if (this.themeState$.theme === 'dark') {
