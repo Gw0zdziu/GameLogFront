12e130aea1ae28a26ae3889f735ac6029d09fbf4
Gw0zdziu <gw0zdziu09@gmail.com>
2026-05-11 12:34:06 +0200

    chore(image-game): create `ImageGameComponent` with optimized image handling and change detection setup

---
+++ src/app/features/game/components/shared/image-game/image-game.component.ts
@@ -0,0 +1,18 @@
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {ProgressSpinner} from 'primeng/progressspinner';
+import {NgOptimizedImage} from '@angular/common';
+import {SafeValue} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-image-game',
+  imports: [
+    ProgressSpinner,
+    NgOptimizedImage
+  ],
+  templateUrl: './image-game.component.html',
+  styleUrl: './image-game.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ImageGameComponent {
+    readonly imageUrl = input<string | SafeValue>('')
+}
