e5049a4c2a890306ca00086370c53923c2fec707
Gw0zdziu <gw0zdziu09@gmail.com>
2026-05-14 19:22:33 +0200

    test(logged-store): add unit tests for `LoggedStoreService` including `isLogged$` and `setLogged` methods

--- src/app/core/store/logged-store/logged-store.service.spec.ts
+++ src/app/core/store/logged-store/logged-store.service.spec.ts
@@ -1,6 +1,5 @@
-import {TestBed} from '@angular/core/testing';
-
-import {LoggedStoreService} from './logged-store.service';
+import { TestBed } from '@angular/core/testing';
+import { LoggedStoreService } from './logged-store.service';
 
 describe('LoggedStoreService', () => {
   let service: LoggedStoreService;
@@ -10,7 +9,32 @@ describe('LoggedStoreService', () => {
     service = TestBed.inject(LoggedStoreService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('initialization', () => {
+    it('isLogged$ starts as false', () => {
+      expect(service.isLogged$()).toBe(false);
+    });
+  });
+
+  describe('setLogged()', () => {
+    it('sets isLogged$ to true', () => {
+      service.setLogged(true);
+
+      expect(service.isLogged$()).toBe(true);
+    });
+
+    it('sets isLogged$ to false', () => {
+      service.setLogged(true);
+      service.setLogged(false);
+
+      expect(service.isLogged$()).toBe(false);
+    });
+
+    it('reflects the last value when called multiple times', () => {
+      service.setLogged(true);
+      service.setLogged(false);
+      service.setLogged(true);
+
+      expect(service.isLogged$()).toBe(true);
+    });
   });
 });
