23ec26c8e8a95a8e2fe8f703c0f9bbfea4d69817
przemekgwozdz <gw0zdziu09@gmail.com>
2026-05-29 17:43:35 +0200

    refactor(category-add): add FontAwesome faSpinner icon for loading states

--- src/app/features/category/components/category-add/category-add.component.ts
+++ src/app/features/category/components/category-add/category-add.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import { CategoryPostDto } from '../../models/category-post.dto';
-import { DynamicDialogRef } from 'primeng/dynamicdialog';
-import { CategoryStore } from '../../store/category-store';
+import {CategoryPostDto} from '../../models/category-post.dto';
+import {DynamicDialogRef} from 'primeng/dynamicdialog';
+import {CategoryStore} from '../../store/category-store';
 import {ButtonDirective, ButtonLabel} from 'primeng/button';
 import {InputText} from 'primeng/inputtext';
 import {Message} from 'primeng/message';
 import {Textarea} from 'primeng/textarea';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faSpinner} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-category-add',
@@ -18,6 +21,7 @@ import {Textarea} from 'primeng/textarea';
     InputText,
     Message,
     Textarea,
+    FaIconComponent,
   ],
   templateUrl: './category-add.component.html',
   styleUrl: './category-add.component.css',
@@ -27,6 +31,7 @@ export class CategoryAddComponent {
   private dynamicDialogRef = inject(DynamicDialogRef);
   store = inject(CategoryStore)
   private formBuilder = inject(FormBuilder);
+  faSpinner = faSpinner;
   newCategoryForm = this.formBuilder.group({
     categoryName: ['', {
       validators: [Validators.required, Validators.minLength(3)],
