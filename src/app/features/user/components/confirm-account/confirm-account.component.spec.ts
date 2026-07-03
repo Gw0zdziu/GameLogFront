bc5d75d1f83b7cebf3c74b3e9f3906b41d1817a0
przemekgwozdz <gw0zdziu09@gmail.com>
2026-07-03 17:34:22 +0200

    test(confirm-account): improve test coverage for confirm and resend actions
    Enhanced test cases for `confirmUser` and `resendConfirmationCode` methods to cover success and error scenarios. Added checks for `ViewContainerRef` clearing and dynamic component rendering logic.

--- src/app/features/user/components/confirm-account/confirm-account.component.spec.ts
+++ src/app/features/user/components/confirm-account/confirm-account.component.spec.ts
@@ -3,24 +3,28 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {ConfirmAccountComponent} from './confirm-account.component';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../services/user.service';
-import {of} from 'rxjs';
+import {of, throwError} from 'rxjs';
+import {ViewContainerRef} from '@angular/core';
 
 describe('ConfirmAccountComponent', () => {
   const mockUserId = '123456';
   let component: ConfirmAccountComponent;
   let fixture: ComponentFixture<ConfirmAccountComponent>;
-  type mockUserServiceType = Pick<UserService, 'confirmUser' | 'resendConfirmationCode'>
   type mockRouterType = Pick<Router, 'navigate'>;
-  let mockUserService: mockUserServiceType;
+  let mockUserService: jest.Mocked<UserService>;
   let mockRouter: mockRouterType;
+  let mockViewContainerRef: jest.Mocked<Partial<ViewContainerRef>>;
 
   beforeEach(async () => {
-    mockUserService =  {
-      confirmUser: jest.fn().mockReturnValue(of(void 0)),
-      resendConfirmationCode: jest.fn().mockReturnValue(of())
-    }
     mockRouter = {
       navigate: jest.fn()
+    }
+    mockUserService ={
+      confirmUser: jest.fn(),
+      resendConfirmationCode: jest.fn()
+    } as unknown as jest.Mocked<UserService>;
+    mockViewContainerRef = {
+      clear: jest.fn()
     }
     await TestBed.configureTestingModule({
       imports: [ConfirmAccountComponent],
@@ -39,6 +43,9 @@ describe('ConfirmAccountComponent', () => {
         },
         {
           provide: UserService, useValue: mockUserService
+        },
+        {
+          provide: ViewContainerRef , useValue: mockViewContainerRef
         }
       ]
     })
@@ -63,26 +70,59 @@ describe('ConfirmAccountComponent', () => {
   describe('submit()', () => {
 
     it('should call confirmUser method from UserService', () => {
+      mockUserService.confirmUser.mockReturnValue(of(void 0));
       component.otpInputControl.setValue('8373');
       component.submit();
       expect(mockUserService.confirmUser).toHaveBeenCalledTimes(1);
     })
 
     it('should don`t call confirmUser when otpInputControl is empty', () => {
+      mockUserService.confirmUser.mockReturnValue(of(void 0));
       component.otpInputControl.setValue('');
       component.submit();
       expect(mockUserService.confirmUser).toHaveBeenCalledTimes(0)
     });
 
     it('should change isSubmit on false when successful method', () => {
+      mockUserService.confirmUser.mockReturnValue(of(void 0));
       component.otpInputControl.setValue('8373');
       component.submit();
-      mockUserService.confirmUser(component.userId, component.otpInputControl.value as string).subscribe(x => {
+      mockUserService.confirmUser(component.userId, component.otpInputControl.value as string).subscribe(() => {
         expect(component.isSubmit()).toBeFalsy();
         expect(mockRouter.navigate).toHaveBeenCalledWith(['/login']);
       })
     });
+
+    it('should change isSubmit when method return error', () => {
+      mockUserService.confirmUser.mockReturnValue(throwError(() => new Error('Error')));
+      component.otpInputControl.setValue('8373');
+      component.submit();
+      expect(component.isSubmit()).toBeFalsy();
+    });
   })
 
+  describe('resendCode()', () => {
+    it('should call resendConfirmationCode method from UserService', () => {
+      mockUserService.resendConfirmationCode.mockReturnValue(of());
+      component.resendCode();
+      expect(mockUserService.resendConfirmationCode).toHaveBeenCalledTimes(1);
+    })
 
+    it('should clear components from viewContainer', () => {
+      mockUserService.resendConfirmationCode.mockReturnValue(of(void 0));
+      component.resendCode();
+      mockUserService.resendConfirmationCode(component.userId).subscribe(() => {
+        expect(mockViewContainerRef.clear).toHaveBeenCalledTimes(1);
+      })
+    });
+
+    it('should call createResendCodeButton method', () => {
+      mockUserService.resendConfirmationCode.mockReturnValue(of(void 0));
+      component.resendCode();
+      mockUserService.resendConfirmationCode(component.userId).subscribe(() => {
+        expect(component.createResendCodeButton).toHaveBeenCalledTimes(1);
+      })
+    });
+
+  })
 });
