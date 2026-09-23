ab23d57c66eb5bbb88015649516136765d538299
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-23 18:21:54 +0200

    refactor(refresh-token): remove unused operators and simplify `refreshToken` logic

--- src/app/core/services/refresh-token/refresh-token.service.ts
+++ src/app/core/services/refresh-token/refresh-token.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {finalize, Observable, shareReplay} from 'rxjs';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +12,9 @@ export class RefreshTokenService {
   private isRefreshed$: Observable<string> | null = null
 
   refreshToken(): Observable<string> {
-    if (!this.isRefreshed$) {
-      this.isRefreshed$ = this.httpClient.post(`${this.apiUrl}/refresh-token`, {}, {
+      return this.httpClient.post(`${this.apiUrl}/refresh-token`, {}, {
         withCredentials: true,
         responseType: 'text'
-      }).pipe(
-        finalize(() => this.isRefreshed$ = null),
-        shareReplay(1)
-      );
+      })
     }
-    return this.isRefreshed$;
-  }
 }
