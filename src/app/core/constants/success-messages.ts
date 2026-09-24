f563b0304d8f7e245a01eb4bb6e829207ed31c21
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-24 20:07:28 +0200

    feat(messages): add success message for category creation, fix key format in `successMessages` map

--- src/app/core/constants/success-messages.ts
+++ src/app/core/constants/success-messages.ts
@@ -1,3 +1,5 @@
 export const successMessages = new Map<string, string>([
-  ['auth.successfully.logout', $localize`Pomyślnie wylogowano`]
+  ['auth.successfully-logout', $localize`Pomyślnie wylogowano`],
+  ['category.successfully-created', $localize`Pomyślnie utworzono nową kategorie`]
+
 ])
